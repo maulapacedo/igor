4075e683ddf97d07416cf206f0962a34f5ff1571
Paula Macedo <100442196+maulapacedo@users.noreply.github.com>
2022-08-24 13:03:05 -0300

    Update init.R

--- init.R
+++ init.R
@@ -4,8 +4,9 @@
 
 my_packages <- c("shiny", "data.table", "dplyr", "ggplot2", "plotly", "shinyWidgets", "shinydashboard", 
                 "readr", "lubridate", "DT", "gt", "htmlTable", "kableExtra", "magrittr", "skimr",
-                "gtsummary", "readxl", "summarytools")
+                "gtsummary", "readxl")
 
+install.packages("summarytools")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
