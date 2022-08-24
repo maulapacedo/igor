f1be8b84f648457b7b6ebba455dcdceb65381d1b
Paula Macedo <100442196+maulapacedo@users.noreply.github.com>
2022-08-24 12:38:10 -0300

    Add files via upload

---
+++ init.R
@@ -0,0 +1,16 @@
+#init.R
+#
+#Example R code to install packages if not already installed 
+
+my_packages = c("shiny", "data.table", "dplyr", "ggplot2", "plotly", "shinyWidgets", "shinydashboard", 
+                "readr", "lubridate", "DT", "gt", "htmlTable", "kableExtra", "magrittr", "skimr",
+                "gtsummary", "readxl", "summarytools")
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, clean=TRUE, quiet=TRUE)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
